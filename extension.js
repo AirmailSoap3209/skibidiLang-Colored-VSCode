3fe21dfecc533a7070897eb451be4c3add57a90f
airmailsoap3209 <airmailsoap3209@gmail.com>
2024-11-27 16:31:12 -0700

    initial commit

---
+++ extension.js
@@ -0,0 +1,13 @@
+// The module 'vscode' contains the VS Code extensibility API
+const vscode = require('vscode');
+
+function activate(context) {
+    console.log('SkibidiLang extension is now active!');
+}
+
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+}
